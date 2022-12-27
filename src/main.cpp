3b360d8ac303b4df48eff38207177fc33c244381
Mayolov <danyvalencia25@gmail.com>
2022-12-26 20:37:02 -0800

    Made the ESP32 a access points

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,10 @@
 const char* ssid = "7LeavesCafe";
 const char* password = "berry129";
 
+// soft webserver thing
+const char* ssidServer = "HASH-SLINGING-SLASHER";
+const char* passwordServer = NULL;
+
 // Set timezone PST and the server we're using 
 #define NTP_OFFSET 16*60*60
 #define NTP_ADDRESS "us.pool.ntp.org"
@@ -25,6 +29,20 @@ const int speedPin = 14;
 const int dir1 = 27;
 const int dir2 = 26;
 int mSpeed = 255;
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Auxiliar variables to store the current output state
+String output25State = "off";
+String output33State = "off";
+
+// Wifi control stuff
+const int output25 = 25;
+const int output33 = 33;
 
 /* These variable take integer values and are assigned to checking variables
    The though of this is when in the loop if a variable is changed a checker var
@@ -45,9 +63,6 @@ int alarmCloseHoursCheck = alarmCloseHours;
 int alarmCloseMins = 45;
 int alarmCloseMinsCheck = alarmCloseMins;
 //***************************************
-
-AlarmId idAlarm1;
-AlarmId idAlarm2; 
 
 // These are for the while loops that move the motor for a specific amount of time
 unsigned long time_since_last_reset = 0;
@@ -142,7 +157,25 @@ Serial.println((newtime.substring(3,5)).toInt());
 Serial.print("Seconds : "); 
 Serial.println((newtime.substring(6,8)).toInt()); 
 Serial.println(timeClient.getFormattedTime());
-setTime((newtime.substring(0,2)).toInt(),(newtime.substring(3,5)).toInt(),(newtime.substring(6,8)).toInt(),1,1,20);
+setTime((newtime.substring(0,2)).toInt(),(newtime.substring(3,5)).toInt(),(newtime.substring(6,8)).toInt(),1,1,20);  // Initialize the output variables as outputs
+
+
+//************************************************************************************************
+//*****************************************Separation*********************************************
+//************************************************************************************************
+// Connect to Wi-Fi network with SSID and password
+Serial.print("Setting AP (Access Point)…");
+// Remove the password parameter, if you want the AP (Access Point) to be open
+WiFi.softAP(ssidServer, passwordServer); //HERECOM BACKAOMEWFNUBOGB#&*B#@*&OG#B*&GB#&GB@&*G#B#&G*BG@&*BG#
+
+IPAddress IP = WiFi.softAPIP();
+Serial.print("AP IP address: ");
+Serial.println(IP);
+  
+server.begin();
+//************************************************************************************************
+//*****************************************Separation*********************************************
+//************************************************************************************************
 
 // alarm one goes clockwise
 // could be made into a function instead
@@ -164,7 +197,51 @@ setTime((newtime.substring(0,2)).toInt(),(newtime.substring(3,5)).toInt(),(newti
 
 void loop()
 {
+
   timeClient.update();
+
+//************************************************************************************************
+//*****************************************Separation*********************************************
+//************************************************************************************************
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+  }
+  // Clear the header variable
+  header = "";
+  // Close the connection
+  client.stop();
+  Serial.println("Client disconnected.");
+  Serial.println("");
+//************************************************************************************************
+//*****************************************Separation*********************************************
+//************************************************************************************************
   Serial.print("Waiting for alarms\n ");
   // If the updated times on the non-Check vars are changed then go and changed their alarm
   if(hour() == alarmOpenHoursCheck && minute() == alarmOpenMinsCheck && isOpen == true){
@@ -192,7 +269,7 @@ void loop()
   // displays the time in minutes
   digitalClockDisplay();
 
-  Alarm.delay(1000);
+  Alarm.delay(20000);
 
   // Psuedo code for the open or close on the app
   /* if sense close button && isOpen ==True{
